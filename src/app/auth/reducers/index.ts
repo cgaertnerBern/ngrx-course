a5f7e30a76f4a12eb2b6388eead24d206647bd61
Christian Gaertner <christiangaertner@gmail.com>
2023-10-13 08:23:05 +0200

    Section 2 Done

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,34 @@
+import {isDevMode} from '@angular/core';
+import {
+  Action,
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector, createReducer,
+  createSelector,
+  MetaReducer, on
+} from '@ngrx/store';
+import {User} from "../model/user.model";
+import {AuthActions} from "../action-types";
+
+export const authFeatureKey = 'auth';
+
+export interface AuthState {
+  user: User
+}
+
+export const initialAuthState = {
+  user: undefined
+}
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+      return {
+        user: action.user
+      };
+    }
+  ),
+  on(AuthActions.logout, (state, action) => {
+    return initialAuthState;
+  })
+)
